c62f49abb0a56b360b69237dd0dc82de08dc5399
Corey Bouton <admin@cbouton.com>
2015-06-14 01:05:02 -0400

    update lib/LocalServices.pm
    Signed-off-by: Corey Bouton <admin@cbouton.com>

--- lib/LocalServices.pm
+++ lib/LocalServices.pm
@@ -12,7 +12,6 @@ sub startup {
 	    my $controller = shift;	    
 	    $controller->render(template => 'index', format => 'html');
 	});
-<<<<<<< HEAD
 	
     $router->get(
         '/search' => sub {
@@ -26,16 +25,7 @@ sub startup {
 	    $controller->render(template => 'results', format => 'html');
 	});
     
-=======
-	    
->>>>>>> dbf78e9543cbfd5f6890ce6696bb57254101fac2
     $router->get(
-        '/testpage' => sub {
-	    my $controller = shift;
-	    $controller->render(template => 'testpage', format => 'html');
-	});
-	
-	    $router->get(
         '/search' => sub {
 	    my $controller = shift;
 	    $controller->render(template => 'search', format => 'html');
